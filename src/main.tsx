b175e38b0aa50556dea417461757c4fe58dd82d1
warugaki <warugaki.web.developer@gmail.com>
2022-11-20 23:15:12 +0900

    add: path alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Global } from '@emotion/react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from '@/App'
 
-import globalStyle from './styles/globalStyle'
+import globalStyle from '@/styles/globalStyle'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
